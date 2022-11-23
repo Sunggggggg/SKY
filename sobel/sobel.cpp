9dc0b264164545b9251d60cd3b281dc8a7320bcd
SKY <sky990319@gmail.com>
2022-11-23 12:14:38 +0900

    sobeledge.avi

--- sobel/sobel.cpp
+++ sobel/sobel.cpp
@@ -31,13 +31,13 @@ int mask_y[SIZE][SIZE] = {
 
 
 int main(int argc, char** argv) {
-	int height, width, n_height, n_width, frame_width, frame_hight;
+	int height, width, n_height, n_width, frame_width, frame_hight, c = 0, max = 30;
 	int x, y, xx, yy;
 	float R_val, G_val, B_val, average_gray;
 	Mat img;
-	VideoCapture cap(0);
-
-	// board
+	VideoCapture cap;
+	cap.open("/dev/video0", CAP_V4L2);
+	
 	if (!cap.isOpened()) {
 		printf("Can`t open Camera\n");
 		return -1;
@@ -45,56 +45,63 @@ int main(int argc, char** argv) {
 
 	frame_width = cap.get(cv::CAP_PROP_FRAME_WIDTH);
 	frame_hight = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+	VideoWriter video("sobel.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 10, Size(frame_width, frame_hight));
 
 	printf("Open Camera\n");
-	cap.read(img);
-	if (img.empty()) {
-		printf("Can`t read Image\n");
-		return -1;
-	}
 	
-	height = img.rows;
-	width = img.cols;
-	Mat imgGray(height, width, CV_8UC1);
-
-	for (y = 0; y < height; y++) {
-		for (x = 0; x < width; x++) {
-			R_val = img.at<Vec3b>(y, x)[2];
-			G_val = img.at<Vec3b>(y, x)[1];
-			B_val = img.at<Vec3b>(y, x)[0];
-
-			average_gray = (int)((R_val + G_val + B_val) / 3);
-
-			imgGray.at<uchar>(y, x) = average_gray;
+	while (c <= max){
+		cap.read(img);
+		if (img.empty()) {
+			printf("Can`t read Image\n");
+			return -1;
 		}
-	}
-	
-	Mat imgSobel(height, width, CV_8UC1);
-	float sumx, sumy, sum;
 
-	for (y = 0; y < height; y++) {
-		for (x = 0; x < width; x++) {
-			sumx = 0, sumy = 0, sum = 0;
-			
-			for (yy = y - SIZE / 2; yy <= y + SIZE / 2; yy++) {
-				for (xx = x - SIZE / 2; xx <= x + SIZE / 2; xx++) {
-					if (yy >= 0 && yy < height && xx >= 0 && xx < width) {
-						sumx += imgGray.at<uchar>(yy, xx) * mask_x[(yy - (y - SIZE / 2))][(xx - (x - SIZE / 2))];
-						sumy += imgGray.at<uchar>(yy, xx) * mask_y[(yy - (y - SIZE / 2))][(xx - (x - SIZE / 2))];
+		height = img.rows;
+		width = img.cols;
+		Mat imgGray(height, width, CV_8UC1);
+
+		for (y = 0; y < height; y++) {
+			for (x = 0; x < width; x++) {
+				R_val = img.at<Vec3b>(y, x)[2];
+				G_val = img.at<Vec3b>(y, x)[1];
+				B_val = img.at<Vec3b>(y, x)[0];
+
+				average_gray = (int)((R_val + G_val + B_val) / 3);
+
+				imgGray.at<uchar>(y, x) = average_gray;
+			}
+		}
+
+		Mat imgSobel(height, width, CV_8UC1);
+		float sumx, sumy, sum;
+
+		for (y = 0; y < height; y++) {
+			for (x = 0; x < width; x++) {
+				sumx = 0, sumy = 0, sum = 0;
+
+				for (yy = y - SIZE / 2; yy <= y + SIZE / 2; yy++) {
+					for (xx = x - SIZE / 2; xx <= x + SIZE / 2; xx++) {
+						if (yy >= 0 && yy < height && xx >= 0 && xx < width) {
+							sumx += imgGray.at<uchar>(yy, xx) * mask_x[(yy - (y - SIZE / 2))][(xx - (x - SIZE / 2))];
+							sumy += imgGray.at<uchar>(yy, xx) * mask_y[(yy - (y - SIZE / 2))][(xx - (x - SIZE / 2))];
+						}
 					}
 				}
-			}
 
-			sum = sqrt(sumx * sumx + sumy * sumy);
-			
-			if (sum > 255 * 0.9) imgSobel.at<uchar>(y, x) = 255;
-			else imgSobel.at<uchar>(y, x) = 0;
-			
+				sum = sqrt(sumx * sumx + sumy * sumy);
+
+				if (sum > 255 * 0.9) imgSobel.at<uchar>(y, x) = 255;
+				else imgSobel.at<uchar>(y, x) = 0;
+
+			}
 		}
+
+		video.write(imgSobel);
+		c++;
 	}
 
-	imwrite("imgSobel.bmp", imgSobel);
 	cap.release();
-	
+	video.release();
+
 	return 0;
 }
